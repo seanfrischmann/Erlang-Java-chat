2ea4a596a2ebeac02cb16e0bbd0cc0d7b6dc65e6
Sean Frischmann <shcsirf@gmail.com>
2014-05-05 17:49:46 -0400

    hopefully fixed keymember function

--- erlang_version/server.erl
+++ erlang_version/server.erl
@@ -28,7 +28,7 @@ loop(List) ->
 			{From, {connect, Name}} ->
 				case List /= [] of
 					true ->
-						case lists:keymember({Name,pid_to_list(From)},List) of
+						case lists:keymember({Name,pid_to_list(From)},1,List) of
 							true ->
 								From ! {connect,false},
 								loop(List);
